7cb091211c970ea315da0608d18f26c9eb8672f9
nasalina120 <nasalina120@gmail.com>
2026-08-01 15:36:45 +0200

    Multiples of 5 using while and do...while

--- multiples-of-5/multiples-of-5.js
+++ multiples-of-5/multiples-of-5.js
@@ -2,15 +2,22 @@ const START_NUMBER = 10;
 const END_NUMBER = 50;
 let i = START_NUMBER;
 
-for (i = START_NUMBER; i <= END_NUMBER; i++) {
+// for (i = START_NUMBER; i <= END_NUMBER; i++) {
+//   if (i % 5 === 0) {
+//     console.log("i :>> ", i);
+//   }
+// }
+
+// do {
+//   if (i % 5 === 0) {
+//     console.log("i :>> ", i);
+//   }
+//   i++;
+// } while (i <= END_NUMBER);
+
+while (i <= END_NUMBER) {
   if (i % 5 === 0) {
     console.log("i :>> ", i);
   }
+  i++;
 }
-// do {
-//   console.log("i :>> ", i--);
-// } while (i >= END_NUMBER);
-
-// while (i >= END_NUMBER) {
-//   console.log("i :>> ", i--);
-// }
