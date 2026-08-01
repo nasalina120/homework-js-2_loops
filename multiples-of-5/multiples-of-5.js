485e381f7c7f6368e706817084a21fc9456cf348
nasalina120 <nasalina120@gmail.com>
2026-08-01 15:10:05 +0200

    Print numbers  multiples of 5 using for

---
+++ multiples-of-5/multiples-of-5.js
@@ -0,0 +1,16 @@
+const START_NUMBER = 10;
+const END_NUMBER = 50;
+let i = START_NUMBER;
+
+for (i = START_NUMBER; i <= END_NUMBER; i++) {
+  if (i % 5 === 0) {
+    console.log("i :>> ", i);
+  }
+}
+// do {
+//   console.log("i :>> ", i--);
+// } while (i >= END_NUMBER);
+
+// while (i >= END_NUMBER) {
+//   console.log("i :>> ", i--);
+// }
